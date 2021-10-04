17fab34c64f70c918a753ccbc415330bfbab2a46
hirupiyumika <hirupiyumika@gmail.com>
2021-10-04 13:53:59 +0530

    add calender UI design

--- src/component/calendar.jsx
+++ src/component/calendar.jsx
@@ -1,22 +1,25 @@
 import React,{useState, useContext} from 'react';
 import { GoogleContext } from '../context/context';
+import './style.css';
+
 const Calendar = () => {
 
-    const { createCalendarEvent,isAuthorized } = useContext(GoogleContext);
-    const [email, setEmail] = useState("hirupiyumika@gmail.com");
-    const [subject, setSubject] = useState("Booking");
-    const [location, setLocation] = useState("Malabe");
-    const [title, setTitle] = useState("Book a car");
-    const [startDate, setDescription] = useState("2021-11-06T09:00:00-07:00");
+    const { createCalendarEvent } = useContext(GoogleContext);
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [location, setLocation] = useState("");
+    const [title, setTitle] = useState("");
+    const [startDate, setStartDate] = useState("2021-09-07T09:00:00-07:00");
     const [timeZone, setTimeZone] = useState("America/Los_Angeles");
-    const [endDate, setEndDate] = useState("2021-11-06T17:00:00-07:00");
+    const [endDate, setEndDate] = useState("2021-09-08T09:00:00-08:00");
 
-    const handleCalendarEvent = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
         createCalendarEvent(genarateCalendarData(),()=>{
-            console.log('Google event is created!');
+            console.log('Google event eka create una!');
     
         })
-      };
+    }
 
     //   Calendar data generation
 
@@ -53,9 +56,35 @@ const Calendar = () => {
 
     return ( 
         <>
-        <p>calander</p>
-        <button onClick={handleCalendarEvent} >ADD CALENDAR EVENT</button>
-        {isAuthorized}
+                    <div className="registration-form">
+            <form onSubmit={onSubmit}>
+            <h3 style={{textAlign:"center"}}>You Are Free To Book</h3>
+            <br/>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="Location" value={location} onChange={(e) => setLocation(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="Start Date" value={startDate} 
+                    onChange={(e) => setStartDate(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control item" placeholder="End Date" value={endDate} 
+                    onChange={(e) => setEndDate(e.target.value)}/>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block create-account">BOOKING</button>
+                </div>
+            </form>
+        </div>
         </>
      );
 }
